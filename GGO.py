d33a681a2e93f88b22905809dc79a94c5db46def
Daniel Barter <danielbarter@gmail.com>
2021-10-01 10:32:07 -0700

    explicit cartesian coordinate differentials

--- GGO.py
+++ GGO.py
@@ -23,6 +23,16 @@ class ConfigurationSpace:
                 sp.Symbol('x_1^' + str(i)),
                 sp.Symbol('x_2^' + str(i))]
 
+        self.cartesian_coordinate_differentials = {}
+
+        for i in range(M):
+            for symbol in self.cartesian_coordinate_symbol[i]:
+                self.cartesian_coordinate_differentials[i] = [
+                    sp.Symbol('dx_0^' + str(i)),
+                    sp.Symbol('dx_1^' + str(i)),
+                    sp.Symbol('dx_2^' + str(i))]
+
+
         # we represent a distance invariant function as a tuple (i,j)
         # where i < j. we represent an angle invariant function as s
         # tuble (base,i,j) where i < j
@@ -60,69 +70,33 @@ class ConfigurationSpace:
 
 
         self.distance_differential = {}
-        self.angle_differential = {}
-
-
         for distance_function in self.distance_expression:
-            differential = []
+            accumulator = 0
             for i in self.cartesian_coordinate_symbol:
-                for symbol in self.cartesian_coordinate_symbol[i]:
-                    differential.append(sp.diff(
+                for symbol, differential in zip(
+                        self.cartesian_coordinate_symbol[i],
+                        self.cartesian_coordinate_differentials[i]):
+
+                    accumulator += sp.diff(
                         self.distance_expression[distance_function],
-                        symbol))
+                        symbol) * differential
 
-            self.distance_differential[distance_function] = differential
+            self.distance_differential[distance_function] = accumulator
 
 
 
+        self.angle_differential = {}
         for angle_function in self.angle_expression:
-            differential = []
+            accumulator = 0
             for i in self.cartesian_coordinate_symbol:
-                for symbol in self.cartesian_coordinate_symbol[i]:
-                    differential.append(sp.diff(
+                for symbol, differential in zip(
+                        self.cartesian_coordinate_symbol[i],
+                        self.cartesian_coordinate_differentials[i]):
+
+                    accumulator += sp.diff(
                         self.angle_expression[angle_function],
-                        symbol))
+                        symbol) * differential
 
-            self.angle_differential[angle_function] = differential
+            self.angle_differential[angle_function] = accumulator
 
-
-class ConfigurationSpacePoint:
-    """
-    class representing points in a configuration space C
-    coordinates are a numpy array
-    """
-    def __init__(self, configuration_space, coordinates):
-
-        if coordinates.shape != (3 * configuration_space.M,):
-            raise Exception("coordinates have wrong shape")
-
-        for (i,j) in combinations(range(configuration_space.M), 2):
-            if (coordinates[3 * i : 3 * i + 3] ==
-                coordinates[3 * j : 3 * j + 3]).all():
-                raise Exception("not a valid point in configuration space")
-
-
-        self.configuration_space = configuration_space
-        self.coordinates = coordinates
-
-
-class QuotientSpacePoint:
-    """
-    class representing points in the quotient space C / G
-    chart is a list of invariant functions
-    coordinates are a numpy array
-    """
-
-    def __init__(self, configuration_space, chart, coordinates):
-
-        if len(chart) != 3 * configuration_space.M - 6:
-            raise Exception("wrong number of invariant functions")
-
-        if coordinates.shape != (3 * configuration_space.M - 6,):
-            raise Exception("coordinates have wrong shape")
-
-
-        self.configuration_space = configuration_space
-        self.chart = chart
-        self.coordinates = coordinates
 
