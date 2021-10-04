42e2bb31e67cc64aa0d32518b538ea24d17a0f26
Daniel Barter <danielbarter@gmail.com>
2021-10-03 21:00:29 -0700

    big refactor so we only need to do big symbolic computations once

--- GGO.py
+++ GGO.py
@@ -11,6 +11,7 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
 def matrix_map(matrix, func, num_threads=8):
     (m,n) = matrix.shape
     entry_list = []
@@ -78,7 +79,180 @@ def maurer_cartan_constructor():
         maurer_cartan_form)
 
 
-(euclidean_matrix_coordinate, maurer_cartan_form) = maurer_cartan_constructor()
+def exterior_derivative(expression, symbols):
+    accumulator = 0
+    for symbol in symbols:
+        accumulator += sp.diff(
+            expression,
+            symbol) * d(symbol)
+
+    return accumulator
+
+
+def distance_expression(symbols_0, symbols_1):
+    displacement_vector = (
+        sp.Matrix([symbols_0]) -
+        sp.Matrix([symbols_1]))
+
+    return displacement_vector.dot(displacement_vector)
+
+
+def angle_expression(symbols_base, symbols_0, symbols_1):
+    displacement_vector_1 = (
+        sp.Matrix([symbols_0]) -
+        sp.Matrix([symbols_base]))
+
+    displacement_vector_2 = (
+        sp.Matrix([symbols_1]) -
+        sp.Matrix([symbols_base]))
+
+
+    return displacement_vector_1.dot(displacement_vector_2)
+
+
+def moving_frame_expression(symbols_base, symbols_0, symbols_1):
+    base_vector = sp.transpose(sp.Matrix([symbols_base]))
+
+    displacement_vector_1 = (
+        sp.Matrix([symbols_0]) -
+        sp.Matrix([symbols_base])
+    )
+
+    displacement_vector_2 = (
+        sp.Matrix([symbols_1]) -
+        sp.Matrix([symbols_base])
+    )
+
+
+    column_1_unnormalized = displacement_vector_1
+    column_2_unnormalized = displacement_vector_1.cross(displacement_vector_2)
+
+    column_1 = column_1_unnormalized / sp.sqrt(
+        column_1_unnormalized.dot(column_1_unnormalized))
+
+    column_2 = column_2_unnormalized / sp.sqrt(
+        column_2_unnormalized.dot(column_2_unnormalized))
+
+
+    column_3 = column_1.cross(column_2)
+
+    rotation_matrix = sp.transpose(
+        sp.Matrix(
+            [column_1,
+             column_2,
+             column_3]))
+
+    full_matrix = rotation_matrix.row_join(base_vector).col_join(
+        sp.Matrix([[0,0,0,1]])
+    )
+
+    return matrix_map(full_matrix, simplify)
+
+
+def distance_differential(distance_expression, symbols_0, symbols_1):
+    return exterior_derivative(
+        distance_expression,
+        symbols_0 + symbols_1)
+
+
+def angle_differential(angle_expression, symbols_base, symbols_0, symbols_1):
+    return exterior_derivative(
+        angle_expression,
+        symbols_base + symbols_0 + symbols_1)
+
+
+def moving_frame_differential(
+        moving_frame_expression,
+        symbols_base,
+        symbols_0,
+        symbols_1):
+
+    (euclidean_matrix_coordinate, maurer_cartan_form) = maurer_cartan_constructor()
+    moving_frame_differential = maurer_cartan_form
+
+    for i in range(3):
+        for j in range(4):
+            symbol = euclidean_matrix_coordinate[i,j]
+
+            moving_frame_differential = moving_frame_differential.subs(
+                d(symbol),
+                exterior_derivative(
+                    moving_frame_expression[i,j],
+                    symbols_base + symbols_0 + symbols_1
+                )
+            )
+
+    for i in range(3):
+        for j in range(4):
+            symbol = euclidean_matrix_coordinate[i,j]
+            moving_frame_differential = moving_frame_differential.subs(
+                symbol, moving_frame_expression[i,j])
+
+
+    return matrix_map(
+        moving_frame_differential,
+        simplify)
+
+
+class ConfigurationSpaceTemplate:
+    """
+    class representing a configuration space on 3 points
+
+    C = {x, y, z in R3 where x != y, x != z and y != z}
+    """
+
+    def __init__(self):
+        self.cartesian_coordinates = {
+            0 : [
+                sp.Symbol('x_0'),
+                sp.Symbol('x_1'),
+                sp.Symbol('x_2')],
+            1 : [
+                sp.Symbol('y_0'),
+                sp.Symbol('y_1'),
+                sp.Symbol('y_2')],
+            2 : [
+                sp.Symbol('z_0'),
+                sp.Symbol('z_1'),
+                sp.Symbol('z_2')],
+        }
+
+        self.distance_expression = distance_expression(
+            self.cartesian_coordinates[0],
+            self.cartesian_coordinates[1])
+
+        self.angle_expression = angle_expression(
+            self.cartesian_coordinates[0],
+            self.cartesian_coordinates[1],
+            self.cartesian_coordinates[2])
+
+        self.moving_frame_expression = moving_frame_expression(
+            self.cartesian_coordinates[0],
+            self.cartesian_coordinates[1],
+            self.cartesian_coordinates[2])
+
+        self.distance_differential = distance_differential(
+            self.distance_expression,
+            self.cartesian_coordinates[0],
+            self.cartesian_coordinates[1])
+
+        self.angle_differential = angle_differential(
+            self.angle_expression,
+            self.cartesian_coordinates[0],
+            self.cartesian_coordinates[1],
+            self.cartesian_coordinates[2])
+
+        self.moving_frame_differential = moving_frame_differential(
+            self.moving_frame_expression,
+            self.cartesian_coordinates[0],
+            self.cartesian_coordinates[1],
+            self.cartesian_coordinates[2])
+
+
+
+
+
+
 
 
 class ConfigurationSpace:
@@ -88,10 +262,12 @@ class ConfigurationSpace:
     C = { x^0, x^1, ..., x^{M-1} in R3 where x^i != x^j }
     """
 
-    def __init__(self, M):
+    def __init__(self, M, template):
 
         self.M = M
         self.cartesian_coordinate = {}
+        self.template = template
+
 
         for i in range(M):
             self.cartesian_coordinate[i] = [
@@ -143,14 +319,18 @@ class ConfigurationSpace:
         if distance_function not in self._distance_expression:
 
             (i,j) = distance_function
+            template_expression = self.template.distance_expression
 
-            displacement_vector = (
-                sp.Matrix([self.cartesian_coordinate[i]]) -
-                sp.Matrix([self.cartesian_coordinate[j]])
-            )
+            for (template_symbol, symbol) in zip(
+                    self.template.cartesian_coordinates[0] +
+                    self.template.cartesian_coordinates[1],
+                    self.cartesian_coordinates[i] +
+                    self.cartesian_coordinates[j]):
 
-            self._distance_expression[distance_function] = displacement_vector.dot(
-                displacement_vector)
+                template_expression = template_expression.subs(
+                    template_symbol, symbol)
+
+            self._distance_expression[distance_function] = template_expression
 
         return self._distance_expression[distance_function]
 
@@ -167,20 +347,22 @@ class ConfigurationSpace:
         if angle_function not in self._angle_expression:
 
             (base, i, j) = angle_function
+            template_expression = self.template.angle_expression
 
-            displacement_vector_1 = (
-                sp.Matrix([self.cartesian_coordinate[i]]) -
-                sp.Matrix([self.cartesian_coordinate[base]])
-            )
+            for (template_symbol, symbol) in zip(
+                    self.template.cartesian_coordinates[0] +
+                    self.template.cartesian_coordinates[1] +
+                    self.template.cartesian_coordinates[2],
+                    self.cartesian_coordinates[base] +
+                    self.cartesian_coordinates[i] +
+                    self.cartesian_coordinates[j]):
 
-            displacement_vector_2 = (
-                sp.Matrix([self.cartesian_coordinate[j]]) -
-                sp.Matrix([self.cartesian_coordinate[base]])
-            )
+                template_expression = template_expression.subs(
+                    template_symbol, symbol)
 
 
-            self._angle_expression[angle_function] = displacement_vector_1.dot(
-                displacement_vector_2)
+
+            self._angle_expression[angle_function] = template_expression
 
         return self._angle_expression[angle_function]
 
@@ -192,128 +374,99 @@ class ConfigurationSpace:
         self.validate_angle_function(angle_function)
 
         if angle_function not in self._moving_frame_expression:
+
             (base, i, j) = angle_function
+            template_expression = self.template.moving_frame_expression
 
-            base_vector = sp.transpose(sp.Matrix([self.cartesian_coordinate[base]]))
+            for (template_symbol, symbol) in zip(
+                    self.template.cartesian_coordinates[0] +
+                    self.template.cartesian_coordinates[1] +
+                    self.template.cartesian_coordinates[2],
+                    self.cartesian_coordinates[base] +
+                    self.cartesian_coordinates[i] +
+                    self.cartesian_coordinates[j]):
 
-            displacement_vector_1 = (
-                sp.Matrix([self.cartesian_coordinate[i]]) -
-                sp.Matrix([self.cartesian_coordinate[base]])
-            )
+                template_expression = template_expression.subs(
+                    template_symbol, symbol)
 
-            displacement_vector_2 = (
-                sp.Matrix([self.cartesian_coordinate[j]]) -
-                sp.Matrix([self.cartesian_coordinate[base]])
-            )
-
-
-            column_1_unnormalized = displacement_vector_1
-            column_2_unnormalized = displacement_vector_1.cross(displacement_vector_2)
-
-            column_1 = column_1_unnormalized / sp.sqrt(
-                column_1_unnormalized.dot(column_1_unnormalized))
-
-            column_2 = column_2_unnormalized / sp.sqrt(
-                column_2_unnormalized.dot(column_2_unnormalized))
-
-
-            column_3 = column_1.cross(column_2)
-
-            rotation_matrix = sp.transpose(
-                sp.Matrix(
-                    [column_1,
-                     column_2,
-                     column_3]))
-
-            full_matrix = rotation_matrix.row_join(base_vector).col_join(
-                sp.Matrix([[0,0,0,1]])
-            )
-
-            self._moving_frame_expression[angle_function] = matrix_map(
-                full_matrix,
-                simplify
-            )
+            self._moving_frame_expression[angle_function] = template_expression
 
         return self._moving_frame_expression[angle_function]
 
 
-    def exterior_derivative(self, expression):
-        """
-        compute exterior derivative in cartesian coordinates
-       """
-        accumulator = 0
-        for i in self.cartesian_coordinate:
-            for symbol, differential in zip(
-                    self.cartesian_coordinate[i],
-                    d(self.cartesian_coordinate[i])):
-
-                accumulator += sp.diff(
-                    expression,
-                    symbol) * differential
-
-        return accumulator
-
 
     def distance_differential(self, distance_function):
+        self.validate_distance_function(distance_function)
 
         if distance_function not in self._distance_differential:
 
-            distance_expression = self.distance_expression(distance_function)
+            (i,j) = distance_function
+            template_expression = self.template.distance_differential
 
-            self._distance_differential[distance_function] = self.exterior_derivative(
-                distance_expression)
+            for (template_symbol, symbol) in zip(
+                    self.template.cartesian_coordinates[0] +
+                    self.template.cartesian_coordinates[1],
+                    self.cartesian_coordinates[i] +
+                    self.cartesian_coordinates[j]):
+
+                template_expression = template_expression.subs(
+                    template_symbol, symbol)
+
+            self._distance_differential[distance_function] = template_expression
 
         return self._distance_differential[distance_function]
 
 
     def angle_differential(self, angle_function):
 
+        self.validate_angle_function(angle_function)
+
         if angle_function not in self._angle_differential:
 
-            angle_expression = self.angle_expression(angle_function)
+            (base, i, j) = angle_function
+            template_expression = self.template.angle_differential
 
-            self._angle_differential[angle_function] = self.exterior_derivative(
-                angle_expression)
+            for (template_symbol, symbol) in zip(
+                    self.template.cartesian_coordinates[0] +
+                    self.template.cartesian_coordinates[1] +
+                    self.template.cartesian_coordinates[2],
+                    self.cartesian_coordinates[base] +
+                    self.cartesian_coordinates[i] +
+                    self.cartesian_coordinates[j]):
+
+                template_expression = template_expression.subs(
+                    template_symbol, symbol)
+
+
+
+            self._angle_differential[angle_function] = template_expression
+
+
 
         return self._angle_differential[angle_function]
 
 
     def moving_frame_differential(self, angle_function):
+
+        self.validate_angle_function(angle_function)
+
+
         if angle_function not in self._moving_frame_differential:
 
-            moving_frame = self.moving_frame_expression(angle_function)
+            (base, i, j) = angle_function
+            template_expression = self.template.moving_frame_differential
 
-            # this is a reference
-            moving_frame_differential = maurer_cartan_form
+            for (template_symbol, symbol) in zip(
+                    self.template.cartesian_coordinates[0] +
+                    self.template.cartesian_coordinates[1] +
+                    self.template.cartesian_coordinates[2],
+                    self.cartesian_coordinates[base] +
+                    self.cartesian_coordinates[i] +
+                    self.cartesian_coordinates[j]):
 
+                template_expression = template_expression.subs(
+                    template_symbol, symbol)
 
-            for i in range(3):
-                for j in range(4):
-                    symbol = euclidean_matrix_coordinate[i,j]
-
-                    # the sympy subs method creates a copy of the expression
-                    # before substituting. This is nice for us because we
-                    # don't need to reconstruct the maurer-cartan form every time
-                    # we call this method
-                    moving_frame_differential = moving_frame_differential.subs(
-                        d(symbol), self.exterior_derivative(moving_frame[i,j])
-                    )
-
-
-            for i in range(3):
-                for j in range(4):
-                    symbol = euclidean_matrix_coordinate[i,j]
-                    moving_frame_differential = moving_frame_differential.subs(
-                        symbol, moving_frame[i,j])
-
-            self._moving_frame_differential[
-                angle_function] = matrix_map(
-                    moving_frame_differential,
-                    simplify
-                )
-
-
+            self._moving_frame_differential[angle_function] = template_expression
 
         return self._moving_frame_differential[angle_function]
-
-
